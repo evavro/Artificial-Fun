35c7b5a59af3a88f0239fbf07e0afeccf0214b92
Thomas Sell <thomas.j.sell@gmail.com>
2012-04-12 13:04:03 -0400

    added pathfinder hw

---
+++ Pathfinder/defines.h
@@ -0,0 +1,27 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+/////////////////////////////////////////////////////////////////////////
+//
+//		File:   Defines.h
+//
+//		Author: Mat Buckland
+//
+//		Desc:   #defines for the code project 'pathfinder'
+//
+/////////////////////////////////////////////////////////////////////////
+
+#define WINDOW_WIDTH	450
+#define WINDOW_HEIGHT	300
+
+#define MAP_WIDTH		15
+#define MAP_HEIGHT		10
+
+#define CROSSOVER_RATE	0.95
+#define MUTATION_RATE	0.05
+
+#define POP_SIZE		100
+#define CHROMO_LENGTH	70
+#define GENE_LENGTH		2
+
+#endif
